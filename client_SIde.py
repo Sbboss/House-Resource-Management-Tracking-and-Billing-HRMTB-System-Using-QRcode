e9e8de4e21e955ea7190498fae81427d046b7745
Sbboss <30629815+shiva2001@users.noreply.github.com>
2020-02-02 11:46:24 +0530

    Add files via upload

---
+++ client_SIde.py
@@ -0,0 +1,210 @@
+import cv2
+import pyzbar.pyzbar as bar
+import mysql.connector as sql
+from tkinter import *
+from tkinter import ttk
+from PIL import Image, ImageTk
+import requests
+
+dB = sql.connect(host="localhost", username="root", passwd="ni39yqmh", database="houseData")
+
+accessId = dB.cursor()
+accessId.execute("show tables")
+
+hname = []
+
+for i in accessId:
+    hname.append(i[0])
+
+accessId.execute("show tables")
+
+houses = {}
+mNo = [8077049606, 9760778113, 8218872092, 9012887831]
+
+mnoi = 0
+
+for i in accessId:
+    houses[i[0]] = mNo[mnoi]
+    mnoi += 1
+
+print(houses)
+
+
+def ScanFunc():
+    cam = cv2.VideoCapture(0)
+    while True:
+        _, fr = cam.read()
+        decodedObj = bar.decode(fr)
+        for i in decodedObj:
+            cv2.imwrite("C://Users//Shiv//PycharmProjects//Shiv//Hackathon//image.png", fr)
+            print(i)
+            hData = i.data.decode('ascii')
+            cv2.rectangle(fr, (i.rect.left, i.rect.top), (i.rect.left + i.rect.width, i.rect.top + i.rect.height),
+                          (255, 0, 0), 3)
+            return hData
+
+        cv2.imshow('QR code Scanner', fr)
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+    cam.release()
+    cv2.destroyAllWindows()
+
+
+def payment_link(amt):
+    headers = {'Host': 'dashboard.paytm.com',
+               'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:72.0) Gecko/20100101 Firefox/72.0',
+               'Accept': 'application/json', 'Accept-Language': 'en-US,en;q=0.5', 'Accept-Encoding': 'gzip, deflate',
+               'Referer': 'https://dashboard.paytm.com/next/payment-link', 'osVersion': '5.0 (Macintosh)',
+               'client': 'UMP', 'customerId': '39327872', 'Content-Type': 'application/json', 'channel': 'web',
+               'X-XSRF-TOKEN': '117f836c-a091-41bb-85cf-c6ea853af764', 'Origin': 'https://dashboard.paytm.com',
+               'Content-Length': '90', 'Connection': 'close', 'Cookie': 'SESSION=702cbe68-4e62-4ce5-bf41-dc77431c1fbb;'}
+    url = "https://dashboard.paytm.com/api/v1/payment/link"
+    data = '{"amount":"' + str(amt) + '","linkDescription":"Bill Payment","linkName":20722,"previewAmount":"' + str(
+        amt) + '"}'
+    try:
+        return requests.post(url, data=data, headers=headers, timeout=10).json()['shortUrl']
+    except:
+        return "https://www.paytm.com/"
+
+def smsSender(totBill1):
+    url = "https://api.msg91.com/api/v2/sendsms"
+    data = '{"sender": "GNOIDA","route": "4","country": "91","sms": [{"message": "' + "Your has been generated successfully of Rs." + str(
+        totBill1) + ", You can made payment by clicking in the following link " + str(
+        payment_link(totBill1)) + '","to": [' + str(houses[Code]) + ']}]}'
+    headers = {'authkey': "224179A4u1cZx15t5cdd109c", 'content-type': "application/json"}
+    r = requests.post(url, data=data, headers=headers, timeout=10)
+    print(data)
+    print(r.text)
+
+    root.destroy()
+    confR = Tk()
+    confR.title("SMS")
+    img = Image.open("tick.png")
+    photo = ImageTk.PhotoImage(img)
+    Label(confR, image=photo).pack()
+    Label(confR).pack()
+    subLab = Label(confR, text='SMS send \n Successfully!!', font=('Times New Roman', 10)).pack()
+
+    confR.geometry("200x200+500+200")
+    confR.mainloop()
+
+
+def getdta(event=""):
+    _list = root.winfo_children()
+
+    for item in _list:
+        if item.winfo_children():
+            _list.extend(item.winfo_children())
+
+    for item in _list:
+        item.pack_forget()
+
+    global serInp
+    serInp = serInpEnt.get()
+    print(serInp)
+    getData = "SELECT * FROM h1 WHERE serial > 0 and Serial < " + serInp
+    print(getData)
+    accessId.execute(getData)
+    res = accessId.fetchall()
+
+    totGas = 0
+    totWat = 0
+    totElec = 0
+    for i in res:
+        totWat += int(i[3][8:10])
+        totGas += int(i[2][0])
+        totElec += int(i[4])
+
+    gas = "Total Gas Used* =  " + str(totGas) + " KG"
+    water = "Total Water Used* =  " + str(totWat) + " L"
+    elec = "Total Electricity Used* =  " + str(totElec) + " Unit"
+
+    gasBill = "The Cost for Gas Consumed this month = ₹" + str(totGas * 20)
+    watBill = "The Cost for Water Consumed this month = ₹" + str(totWat * 0.95)
+    elecBill = "The Cost for Electricity Consumed this month = ₹" + str(totElec * 4.5)
+    totBill1 = totGas * 20 + totWat * 0.85 + totElec * 4.5
+    totBill = "The amount payable for this month resource consumption = ₹" + str(
+        totGas * 20 + totWat * 0.85 + totElec * 4.5)
+
+    gasLbl = Label(root, text=gas).pack()
+    gasLbl = Label(root).pack()
+    waterLbl = Label(root, text=water).place(x=315, y=20)
+    waterLbl = Label(root).pack()
+    elecLbl = Label(root, text=elec).place(x=315, y=40)
+    elecLbl = Label(root).pack()
+    gasLbl = Label(root, text=gasBill).place(x=0, y=143)
+    gasLbl = Label(root).pack()
+    waterLbl = Label(root, text=watBill).place(x=0, y=169)
+    waterLbl = Label(root).pack()
+    elecLbl = Label(root, text=elecBill).place(x=0, y=195)
+    elecLbl = Label(root).pack()
+    elecLbl = Label(root, text=totBill, bg='yellow').place(x=0, y=225)
+    elecLbl = Label(root).pack()
+
+    elecLbl = Label(root,
+                    text="*You can Pay the amount Now via CC/DC, or through Net Banking etc online payment option").place(
+        x=0, y=265)
+    elecLbl = Label(root).pack()
+    elecLbl = Label(root, text="*You can complain for any kind of mistake or error in out system").place(x=0, y=285)
+    elecLbl = Label(root).pack()
+    elecLbl = Label(root, text="*Sorry for any kind of inconvenience").place(x=0, y=315)
+    elecLbl = Label(root).pack()
+    elecLbl = Label(root, text="Bill Generated by Gnida").place(x=300, y=325)
+    elecLbl = Label(root).pack()
+    elecLbl = Label(root).pack()
+    elecLbl = Label(root).pack()
+    elecLbl = Label(root).pack()
+    elecLbl = Label(root).pack()
+    elecLbl = Label(root).pack()
+    elecLbl = Label(root).pack()
+    elecLbl = Label(root).pack()
+    elecLbl = Label(root).pack()
+    elecLbl = Label(root).pack()
+
+    subLab = Label(root, text='Send SMS', font=('Times New Roman', 10))
+    subLab.place(x=673, y=325)
+
+    serBut.place(x=685, y=290)
+    photo2 = PhotoImage(file='sms.png')
+    serBut.config(image=photo2)
+    serBut.config(command=lambda: smsSender(totBill1))
+
+    root.geometry("768x350+300+0")
+    root.mainloop()
+
+
+Code = ScanFunc()
+cv2.destroyAllWindows()
+if Code in hname:
+    root = Tk()
+    root.title("House Resource Management, Tracking and Billing System 🤩😎")
+    root.iconbitmap(False, "5.ico")
+
+    frane = Frame(root)
+    frane.pack()
+    img = Image.open("image.png")
+    img = img.resize((250, 220))
+    photo = ImageTk.PhotoImage(img)
+    Label(frane, image=photo).pack()
+    Label(frane).pack()
+    Label(frane).pack()
+
+    photo2 = PhotoImage(file='submit.png')
+
+    Label(root, text="Upto how many Days??").pack()
+    serInpEnt = ttk.Entry(root, width=40)
+    serInpEnt.pack()
+    Label(root).pack()
+    subLab = Label(root, text='SUBMIT', font=('Times New Roman', 10))
+    subLab.place(x=95, y=315)
+    serBut = ttk.Button(root, width=40, image=photo2, command=getdta)
+    serBut.place(x=158, y=310)
+    root.bind("<Return>", getdta)
+
+    Label(root).pack()
+
+    root.geometry("300x350+500+200")
+    root.mainloop()
---
+++ server_Side.py
@@ -0,0 +1,134 @@
+import cv2
+import pyzbar.pyzbar as bar
+import mysql.connector as sql
+from tkinter import *
+from tkinter import ttk
+import datetime
+from PIL import Image, ImageTk
+import time
+
+dB = sql.connect(host="localhost", username="root", passwd="ni39yqmh", database="houseData")
+
+accessId = dB.cursor(buffered=True)
+accessId.execute("show tables")
+
+houses = {}
+mNo = [8791622454, 9760778113, 8218872092, 9012887831]
+
+mnoi = 0
+
+for i in accessId:
+    global moni
+    houses[i[0]] = mNo[mnoi]
+    mnoi += 1
+
+print(houses)
+
+dateVar = 0
+gasVar = 0
+waterVar = 0
+elecVar = 0
+
+
+def ScanFunc():
+    cam = cv2.VideoCapture(0)
+
+    while True:
+        _, fr = cam.read()
+        decodedObj = bar.decode(fr)
+        for i in decodedObj:
+            # print("Data =", i.data.decode('ascii'))
+            print(i)
+            hData = i.data.decode('ascii')
+            cv2.rectangle(fr, (i.rect.left, i.rect.top), (i.rect.left + i.rect.width, i.rect.top + i.rect.height),
+                          (255, 0, 0), 3)
+            return hData
+
+        cv2.imshow('fr', fr)
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+    cam.release()
+    cv2.destroyAllWindows()
+
+
+def nextDate():
+    global dateVar
+    dateVar += 1
+    print(dateVar)
+    base = datetime.datetime.today()
+    date = str(base + datetime.timedelta(dateVar))
+    print(date)
+    date = date[0:10]
+    time.sleep(2)
+    accessId.execute("SELECT max(Serial) FROM h1")
+    res = accessId.fetchall()
+    serial = res[0][0] + 1
+    gas = str(gasVar) + " Kg"
+    water = "3-4PM, " + str(waterVar) + "L"
+    a = (serial, date, gas, water, elecVar)
+    incm = "insert into " + Code + "(Serial, date, Gas, Water, Electricity) values(%s,%s,%s,%s,%s)"
+    accessId.execute(incm, a)
+    progBar.insert(INSERT, "Done👍👍\n")
+    dB.commit()
+
+
+def incGas():
+    global gasVar
+    gasVar = gasVar + 1
+    print(gasVar)
+    progBar.insert(INSERT, "Total Gas used till now " + str(gasVar) + " Kg\n")
+
+
+def incWat():
+    global waterVar
+    waterVar = waterVar + 2
+    print(waterVar)
+    progBar.insert(INSERT, "Total Water used till now " + str(waterVar) + " Lt.\n")
+
+
+def incElec():
+    global elecVar
+    elecVar = elecVar + 1
+    print(elecVar)
+    progBar.insert(INSERT, "Total Electricity consumed till now " + str(elecVar) + " Unit\n")
+
+
+Code = ScanFunc()
+cv2.destroyAllWindows()
+if Code in houses:
+    root = Tk()
+    root.title("House Resource Management, Tracking and Billing System 🤩😎")
+    root.iconbitmap(False, "5.ico")
+
+    img = Image.open("bu.jpg")
+    photo = ImageTk.PhotoImage(img)
+    Label(image=photo).pack()
+
+    dateBut = ttk.Button(root, text="Update Date", width=30, command=nextDate)
+    dateBut.place(relx=0.66, rely=0.20, relheight=0.1, relwidth=0.3)
+    gasBut = ttk.Button(root, text="Gas", width=30, command=incGas)
+    gasBut.place(relx=0.66, rely=0.35, relheight=0.1, relwidth=0.3)
+    waterBut = ttk.Button(root, text="Water", width=30, command=incWat)
+    waterBut.place(relx=0.66, rely=0.50, relheight=0.1, relwidth=0.3)
+    elecBut = ttk.Button(root, text="Electricity", width=30, command=incElec)
+    elecBut.place(relx=0.66, rely=0.65, relheight=0.1, relwidth=0.3)
+    exitBut = ttk.Button(root, text="Exit", width=30, command=exit)
+    exitBut.place(relx=0.85, rely=0.88, relheight=0.05, relwidth=0.1)
+
+    # Status Frame
+    satFrame = ttk.LabelFrame(root, text="Progress...")
+    satFrame.place(relx=0.01, rely=0.01, relheight=0.98, relwidth=0.6)
+    progBar = Text(satFrame, wrap=WORD)
+    progBar.config(state="normal")
+    progBar.place(relx=0.05, rely=0.02, relheight=0.97, relwidth=0.9)
+    scrllBar = ttk.Scrollbar(progBar)
+    scrllBar.pack(side=RIGHT, fill=Y)
+    scrllBar.config(command=progBar.yview)
+    scrllBar = ttk.Scrollbar(progBar)
+    # End Status Frame
+
+    root.geometry("1366x768")
+    root.mainloop()
